7b9368dbfc80c3dd9edc3717b75b9a402f0c79ff
Jack van Zyl <jackvanzyl@icloud.com>
2019-04-01 18:47:52 +0200

    Version nr increment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r') as fh:
 
 setuptools.setup(
     name = 'mezzanine_cartridge_api',
-    version = '1.2.9',
+    version = '1.2.10',
     author = 'Jack van Zyl',
     author_email = 'jackvanzyl@icloud.com',
     description = 'A REST Web API for Mezzanine CMS with the Cartridge e-commerce extension',
