8b6c693891689c5b119a5c1e936f7d25d0bfdbb4
Jack van Zyl <jackvanzyl@icloud.com>
2019-04-12 05:01:54 +0200

    Adding in-memory system settings

--- mezzanine_cartridge_api/urls.py
+++ mezzanine_cartridge_api/urls.py
@@ -23,7 +23,7 @@ from . import views
 schema_view = get_schema_view(
    openapi.Info(
       title='Mezzanine API',
-      default_version='1.3.55',
+      default_version='1.3.56',
       description='A REST Web API for the Mezzanine content management system with the Cartridge e-commerce extension.',
    ),
    public=True,
